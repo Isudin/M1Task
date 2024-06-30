961f41b03121ece4f14d8d6659aa3849401e51b9
Isu <natantrojak@wp.pl>
2024-06-30 18:10:16 +0200

    Added BillingEntryContext to support EF

---
+++ M1Task/Infrastructure/BillingEntryContext.cs
@@ -0,0 +1,19 @@
+﻿using M1Task.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace M1Task.Infrastructure;
+
+public class BillingEntryContext : DbContext
+{
+    private readonly static string connectionStringName
+        = "ConnectionStringThatShouldntActuallyBePushedToGithubAndShouldntHaveSuchALongNameAgen";
+    internal DbSet<BillingEntry> Stalkers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+        optionsBuilder.UseSqlServer(config.GetConnectionString(connectionStringName));
+    }
+}
