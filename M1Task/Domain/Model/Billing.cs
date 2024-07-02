9b0b5416f54fd0ad29779e77a3d85b160519dc43
Isu <natantrojak@wp.pl>
2024-07-02 16:11:26 +0200

    Created domain model, dbcontext class and first migration

---
+++ M1Task/Domain/Model/Billing.cs
@@ -0,0 +1,14 @@
+﻿namespace M1Task.Domain.Model;
+
+public class Billing
+{
+    public int Id { get; set; }
+    public DateTime OccurredAt { get; set; }
+    public string TypeId { get; set; } = string.Empty;
+    public string OfferId { get; set; } = string.Empty;
+    public string Amount { get; set; } = string.Empty;
+    public string Currency { get; set; } = string.Empty;
+    public string Tax { get; set; } = string.Empty;
+    public string OrderId { get; set; } = string.Empty;
+
+}
--- M1Task/Infrastructure/BillingEntryContext.cs
+++ M1Task/Infrastructure/BillingEntryContext.cs
@@ -1,4 +1,5 @@
-﻿using M1Task.Model;
+﻿using M1Task.ApiConnection.Model;
+using M1Task.Domain.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
@@ -8,7 +9,7 @@ public class BillingEntryContext : DbContext
 {
     private readonly static string connectionStringName
         = "ConnectionStringThatShouldntActuallyBePushedToGithubAndShouldntHaveSuchALongNameAgen";
-    internal DbSet<BillingEntry> Stalkers { get; set; }
+    internal DbSet<Billing> Billings { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
