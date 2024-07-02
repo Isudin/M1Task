f2a00e704d3909f4608e98fb1e2ffdcfa5e008e1
Isu <natantrojak@wp.pl>
2024-07-02 18:54:42 +0200

    Changed first message showing after running the code

--- M1Task/Program.cs
+++ M1Task/Program.cs
@@ -1,15 +1,15 @@
 ﻿using M1Task.Domain.Billings;
 using System.Text;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 string entryString = "ABE - Allegro Billing Entries\n" +
                      "Select action:\n" +
                      $"{ListCommands()}";
+Console.WriteLine($"{entryString}{Environment.NewLine}" +
+    $"Remember to fill in all necessary connection data in appsettings.json file before proceeding");
 
 var exit = false;
 while (!exit)
 {
-    Console.WriteLine(entryString);
     string? input = Console.ReadLine();
     bool isNumber = int.TryParse(input, out int number);
 
@@ -22,6 +22,7 @@ while (!exit)
     exit = ExecuteCommands(number);
 
     Console.WriteLine($"{Environment.NewLine}-----{Environment.NewLine}");
+    Console.WriteLine(entryString);
 }
 
 return;
