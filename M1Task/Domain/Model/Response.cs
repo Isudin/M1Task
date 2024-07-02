830dbe4464da6b983be64432eb1f05a8c743a8c6
Isu <natantrojak@wp.pl>
2024-07-02 17:48:26 +0200

    Added Response model

---
+++ M1Task/Domain/Model/Response.cs
@@ -0,0 +1,22 @@
+﻿using M1Task.Domain.ApiConnection.Model.Errors;
+
+namespace M1Task.Domain.Model
+{
+    internal class Response
+    {
+        public Response(string message, bool isError = false)
+        {
+            Message = message;
+            IsError = isError;
+        }
+
+        public Response(IError error)
+        {
+            Message = error.ErrorMessage;
+            IsError = true;
+        }
+
+        internal string Message { get; set; } = string.Empty;
+        internal bool IsError { get; set; }
+    }
+}
