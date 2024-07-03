9881ae4d14e78feb0cb66f8e2e9eb55e29e8f2c3
Isu <natantrojak@wp.pl>
2024-07-03 13:09:06 +0200

    Changes in Authorization models

--- M1Task/Domain/Api/Model/AuthorizationResponse.cs
+++ M1Task/Domain/Api/Model/AuthorizationResponse.cs
@@ -2,10 +2,12 @@
 
 namespace M1Task.Domain.Api.Model;
 
-internal class AuthorizationResponse
+internal class UserAuthorization
 {
     [JsonProperty("access_token")]
     public string AccessToken { get; set; }
+    [JsonProperty("refresh_token")]
+    public string RefreshToken { get; set; }
 
     [JsonProperty("token_type")]
     public string TokenType { get; set; }
@@ -21,4 +23,22 @@ internal class AuthorizationResponse
 
     [JsonProperty("jti")]
     public string JTI { get; set; }
+}
+
+
+[JsonObject(MemberSerialization.OptOut)]
+public class DeviceAuthorization
+{
+    [JsonProperty("device_code")]
+    public string DeviceCode { get; set; }
+    [JsonProperty("expires_in")]
+    public string ExpiresIn { get; set; }
+    [JsonProperty("interval")]
+    public int interval { get; set; } = 5;
+    [JsonProperty("verification_uri")]
+    public Uri VerificationUri { get; set; }
+    [JsonProperty("verification_uri_complete")]
+    public Uri VerificationUriComplete { get; set; }
+    [JsonProperty("user_code")]
+    public string UserCode { get; set; }
 }
