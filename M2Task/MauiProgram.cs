bb43cb3a1d12c6e2943da78b5c0466d9c8e42245
Isu <natantrojak@wp.pl>
2024-07-06 17:01:00 +0200

    Registered dbcontext

--- M2Task/MauiProgram.cs
+++ M2Task/MauiProgram.cs
@@ -1,25 +1,27 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using M2Task.Infrastructure;
+using Microsoft.Extensions.Logging;
 
-namespace M2Task
+namespace M2Task;
+
+public static class MauiProgram
 {
-    public static class MauiProgram
+    public static MauiApp CreateMauiApp()
     {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+         builder.Services.AddDbContext<TheOtherContext>();
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+		builder.Logging.AddDebug();
 #endif
 
-            return builder.Build();
-        }
+        return builder.Build();
     }
 }
