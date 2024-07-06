bcefbf91d05dbd8111e9d6d0291540c5821112e7
Isu <natantrojak@wp.pl>
2024-07-06 17:00:14 +0200

    Added dbcontext

---
+++ M2Task/Infrastructure/TheOtherContext.cs
@@ -0,0 +1,20 @@
+﻿using M2Task.Domain.Model.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace M2Task.Infrastructure;
+
+public class TheOtherContext : DbContext
+{
+    private readonly static string connectionStringName
+        = "ConnectionStringThatShouldntActuallyBePushedToGithubAndShouldntHaveSuchALongNameAgen";
+
+    internal DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+        optionsBuilder.UseSqlServer(config.GetConnectionString(connectionStringName));
+    }
+}
