ea4d4f2601315666dabc482c2ff4a9c1cb5c7f29
Isu <natantrojak@wp.pl>
2024-07-06 15:26:51 +0200

    Added core class for merging products data

---
+++ M2Task/Domain/Model/Database/Product.cs
@@ -0,0 +1,25 @@
+﻿namespace M2Task.Domain.Model.Database
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+        public LocalizedText Name { get; set; } = new();
+        public LocalizedText Description { get; set; } = new();
+        public string? PictureUrl { get; set; }
+        public bool IsUseful { get; set; }
+        public Price? Srp { get; set; }
+        public string Ean { get; set; } = string.Empty;
+        public List<int> VariantsIds { get; set; } = [];
+
+        public class LocalizedText
+        {
+            public string Text { get; set; } = string.Empty;
+            public string LanguageCode { get; set; } = "PL";
+        }
+        public class Price
+        {
+            public decimal Amount { get; set; }
+            public string CurrencyCode { get; set; } = "PLN";
+        }
+    }
+}
