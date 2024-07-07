548e50f7b467a33fdfdb34111fdbe113a530227e
Isu <natantrojak@wp.pl>
2024-07-07 18:56:24 +0200

    Added Name and Description properties to Product model

--- M2Task/Domain/Model/Database/Product.cs
+++ M2Task/Domain/Model/Database/Product.cs
@@ -5,6 +5,36 @@
         public int Id { get; set; }
         public string Code { get; set; }
         public string Ean { get; set; } = string.Empty;
+        public LocalizedText Name
+        {
+            get
+            {
+                if (Names.Count == 0) return new("Brak nazwy");
+
+                string[] plCode = ["PL", "POL"];
+                var namePl = Names.FirstOrDefault(x => plCode.Contains(x.LanguageCode.ToUpper()));
+                if (namePl != null)
+                    return namePl;
+
+                return Names.First();
+            }
+        }
+
+        public LocalizedText Description
+        {
+            get
+            {
+                if (Descriptions.Count == 0) return new("Brak opisu");
+
+                string[] plCode = ["PL", "POL"];
+                var descPl = Descriptions.FirstOrDefault(x => plCode.Contains(x.LanguageCode.ToUpper()));
+                if (descPl != null)
+                    return descPl;
+
+                return Descriptions.First();
+            }
+        }
+
         public List<LocalizedText> Names { get; set; } = [];
         public List<LocalizedText> Descriptions { get; set; } = [];
         public List<string> ImageUrls { get; set; } = [];
