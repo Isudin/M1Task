aa4b6f9347955b6b83b4dac45e739914ce3f44a7
Isu <natantrojak@wp.pl>
2024-07-07 16:54:17 +0200

    Created mapping class

---
+++ M2Task/Domain/XmlMapper.cs
@@ -0,0 +1,28 @@
+﻿using M2Task.Domain.Model.Database;
+using M2Task.Domain.Model.Maps;
+using M2Task.Domain.Model.XML;
+
+namespace M2Task.Domain
+{
+    internal class XmlMapper
+    {
+        public static Product[] MapToProducts(IXmlModel xml)
+        {
+            //TODO: Automate mapping
+            if (xml is D1M1Offer d1m1)
+                return D1M1Map.Map(d1m1);
+
+            if (xml is D1M2Offer d1m2)
+                return D1M2Map.Map(d1m2);
+
+            if (xml is D2Products d2)
+                return D2Map.Map(d2);
+
+            if (xml is D3Produkty d3)
+                return D3Map.Map(d3);
+
+
+            return [];
+        }
+    }
+}
