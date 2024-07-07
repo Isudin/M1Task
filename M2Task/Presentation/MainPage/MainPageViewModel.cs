834cbbdc5dd7c7a178f24501871510a59c8f83bc
Isu <natantrojak@wp.pl>
2024-07-07 14:33:23 +0200

    Added basic viewmodel logic for reading xml files

--- M2Task/Presentation/MainPage/MainPageViewModel.cs
+++ M2Task/Presentation/MainPage/MainPageViewModel.cs
@@ -1,9 +1,75 @@
-﻿using System.ComponentModel;
-using System.Runtime.CompilerServices;
+﻿using M2Task.Domain.Model.Database;
+using M2Task.Domain.Model.XML;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace M2Task.Presentation;
 
 public partial class MainPageViewModel
 {
+    public MainPageViewModel()
+    {
+        XmlModels = ListAllXmlModels();
+    }
 
+    public ICommand SelectFilesCommand => new Command(SelectFiles);
+
+    private Type[] XmlModels { get; set; }
+    public ObservableCollection<Product> Products { get; set; } = [];
+
+    public void SelectFiles()
+    {
+        IEnumerable<FileResult> selectedFiles = Task.Run(async () => await FilePicker.Default.PickMultipleAsync()).Result;
+        selectedFiles = selectedFiles.Where(x => x.ContentType == "text/xml");
+        if (!selectedFiles.Any()) return;
+
+        var t = ReadSelectedFiles(selectedFiles);
+    }
+
+    private async Task ReadSelectedFiles(IEnumerable<FileResult> files)
+    {
+        foreach (var file in files)
+        {
+            //if 
+        }
+    }
+
+    private async Task ReadFile(FileResult file)
+    {
+        XmlDocument xml = new();
+        xml.Load(file.OpenReadAsync().GetAwaiter().GetResult());
+        var reader = new XmlNodeReader(xml);
+
+        foreach (Type xmlModel in XmlModels)
+        {
+            object? deserializedFile = DeserializeFile(xmlModel, reader);
+            if (deserializedFile == null) continue;
+
+            MapDeserializedFile(xmlModel, deserializedFile);
+        }
+    }
+
+    private async Task<object?> DeserializeFile(Type xmlModel, XmlNodeReader reader)
+    {
+        XmlSerializer serializer = new(xmlModel);
+        if (!serializer.CanDeserialize(reader)) return null;
+
+        return serializer.Deserialize(reader);
+    }
+
+    private async Task MapDeserializedFile(Type xmlModel, object deserializedFile)
+    {
+
+    }
+
+    private Type[] ListAllXmlModels()
+    {
+        var type = typeof(IXmlModel);
+        return AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(s => s.GetTypes())
+            .Where(type.IsAssignableFrom)
+            .ToArray();
+    }
 }
