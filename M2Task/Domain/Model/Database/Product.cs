27e53aa5c76daade608dfaf5ea38d0e52f081fcb
Isu <natantrojak@wp.pl>
2024-07-07 15:19:22 +0200

    Some changes in Product model

--- M2Task/Domain/Model/Database/Product.cs
+++ M2Task/Domain/Model/Database/Product.cs
@@ -3,23 +3,16 @@
     public class Product
     {
         public int Id { get; set; }
-        public LocalizedText Name { get; set; } = new();
-        public LocalizedText Description { get; set; } = new();
+        public LocalizedText? Name { get; set; }
+        public LocalizedText? Description { get; set; }
         public string? PictureUrl { get; set; }
         public bool IsUseful { get; set; }
         public Price? Srp { get; set; }
         public string Ean { get; set; } = string.Empty;
         public List<int> VariantsIds { get; set; } = [];
 
-        public class LocalizedText
-        {
-            public string Text { get; set; } = string.Empty;
-            public string LanguageCode { get; set; } = "PL";
-        }
-        public class Price
-        {
-            public decimal Amount { get; set; }
-            public string CurrencyCode { get; set; } = "PLN";
-        }
+        public record LocalizedText(decimal Text, string LanguageCode = "PLN") { }
+
+        public record Price(decimal Net, decimal Gross, decimal Vat, string CurrencyCode = "PLN") { }
     }
 }
