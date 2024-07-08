31d950f961bb879201bf286b1eae054a7796d615
Isu <natantrojak@wp.pl>
2024-07-08 18:58:36 +0200

    Serialization adjustments

--- M2Task/Domain/Model/Maps/D2Map.cs
+++ M2Task/Domain/Model/Maps/D2Map.cs
@@ -16,14 +16,18 @@ namespace M2Task.Domain.Model.Maps
                 Product product = new();
                 product.Ean = p.Ean;
                 product.Code = p.Sku;
-                product.Names.Add(new(p.Name));
-                product.Descriptions.Add(new(p.Desc));
                 product.Quantity = p.Qty;
                 AddSrp(p, product);
 
                 if (p.Photos != null)
                     foreach (var photo in p.Photos)
                         product.ImageUrls.Add(photo.Value);
+
+                if (!string.IsNullOrEmpty(p.Name))
+                    product.Names.Add(new(p.Name));
+
+                if (!string.IsNullOrEmpty(p.Desc))
+                    product.Descriptions.Add(new(p.Desc));
 
                 products.Add(product);
             }
@@ -33,10 +37,9 @@ namespace M2Task.Domain.Model.Maps
 
         private static void AddSrp(D2Products.ProductsProduct p, Product product)
         {
-            decimal net = 0, gross = 0, vat = 0;
-            _ = decimal.TryParse(p.PriceAfterDiscountNet, CultureInfo.InvariantCulture, out net);
-            _ = decimal.TryParse(p.RetailPriceGross, CultureInfo.InvariantCulture, out gross);
-            _ = decimal.TryParse(p.Vat, CultureInfo.InvariantCulture, out vat);
+            _ = decimal.TryParse(p.PriceAfterDiscountNet, CultureInfo.InvariantCulture, out decimal net);
+            _ = decimal.TryParse(p.RetailPriceGross, CultureInfo.InvariantCulture, out decimal gross);
+            _ = decimal.TryParse(p.Vat, CultureInfo.InvariantCulture, out decimal vat);
             product.Srp = new(net, gross, vat);
         }
     }
--- M2Task/Domain/XmlMapper.cs
+++ M2Task/Domain/XmlMapper.cs
@@ -8,18 +8,23 @@ namespace M2Task.Domain
     {
         public static Product[] MapToProducts(IXmlModel xml)
         {
-            //TODO: Automate mapping
-            if (xml is D1M1Offer d1m1)
-                return D1M1Map.Map(d1m1);
 
-            if (xml is D1M2Offer d1m2)
-                return D1M2Map.Map(d1m2);
+            try
+            {
+                //TODO: Automate mapping
+                if (xml is D1M1Offer d1m1)
+                    return D1M1Map.Map(d1m1);
 
-            if (xml is D2Products d2)
-                return D2Map.Map(d2);
+                if (xml is D1M2Offer d1m2)
+                    return D1M2Map.Map(d1m2);
 
-            if (xml is D3Produkty d3)
-                return D3Map.Map(d3);
+                if (xml is D2Products d2)
+                    return D2Map.Map(d2);
+
+                if (xml is D3Produkty d3)
+                    return D3Map.Map(d3);
+            }
+            catch { }
 
             return [];
         }
